72284943015621c59c1e8204aaa84deedc5fd828
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-01-13 19:24:28 -0300

    Delete auth.service.d.ts

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { LoginDto, AuthResponse } from './dto/login.dto';
-import { PrismaService } from 'src/prisma.service';
-import { JwtService } from '@nestjs/jwt';
-export declare class AuthService {
-    private database;
-    private jwt;
-    constructor(database: PrismaService, jwt: JwtService);
-    login(data: LoginDto): Promise<AuthResponse>;
-}
