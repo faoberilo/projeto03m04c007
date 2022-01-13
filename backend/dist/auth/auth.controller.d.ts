5bc322e9b114cf7634f0f6a0def803c90c890bef
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-01-13 19:25:05 -0300

    Delete auth.controller.d.ts

--- backend/dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-import { LoginDto, AuthResponse } from './dto/login.dto';
-import { User } from '@prisma/client';
-export declare class AuthController {
-    private service;
-    constructor(service: AuthService);
-    login(data: LoginDto): Promise<AuthResponse>;
-    profile(user: User): User;
-}
