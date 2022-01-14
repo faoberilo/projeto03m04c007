adf23b8fdde0baf2c6e5f4e8024d29fd60ad109c
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-01-14 12:51:59 -0300

    Add files via upload

---
+++ backend/dist/user/dto/createuser.dtp.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=createuser.dtp.js.map
---
+++ backend/dist/user/dto/update-user.dto.d.ts
@@ -0,0 +1,9 @@
+export declare class UpdateUserDto {
+    name: string;
+    nickname: string;
+    email: string;
+    birthdate: string;
+    imageUrl: string;
+    password: string;
+    passwordConfirmation: string;
+}
